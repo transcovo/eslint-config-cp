52aea0d34570708549899a75012737f128ff237c
Samuel Rossille <samuel.rossille@gmail.com>
2017-05-07 00:20:28 +0200

    TECH improve version spec

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = {
   extends: [
     'airbnb/base'
@@ -5,7 +7,6 @@ module.exports = {
   rules: {
     'no-use-before-define': [2, 'nofunc'],
     'vars-on-top': 0,
-    'comma-dangle': 0,
     'no-param-reassign': 0,
     'valid-jsdoc': 1,
     'require-jsdoc': 1,
@@ -13,36 +14,36 @@ module.exports = {
     'no-underscore-dangle': [0],
     'newline-per-chained-call': [0],
     'import/no-extraneous-dependencies': [2, { devDependencies: true }],
-    'strict': [2, 'global'],
+    strict: [2, 'global'],
     'arrow-parens': [0],
     'no-restricted-syntax': [
       'error',
       'ForInStatement',
       'LabeledStatement',
-      'WithStatement',
+      'WithStatement'
     ]
   },
   ecmaFeatures: {
-    'generators': true,
-    'impliedStrict': true
+    generators: true,
+    impliedStrict: true
   },
   globals: {
-    'describe': true,
-    'it': true,
-    'xdescribe': true,
-    'xit': true,
-    'before': true,
-    'after': true,
-    'beforeEach': true,
-    'afterEach': true
+    describe: true,
+    it: true,
+    xdescribe: true,
+    xit: true,
+    before: true,
+    after: true,
+    beforeEach: true,
+    afterEach: true
   },
-  'env': {
-    'es6': true,
-    'node': true,
-    'mocha': true
+  env: {
+    es6: true,
+    node: true,
+    mocha: true
   },
-  'parserOptions': {
-    'ecmaVersion': 6,
-    'sourceType': 'global'
-},
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'global'
+  }
 };
